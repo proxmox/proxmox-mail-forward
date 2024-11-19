proxmox/proxmox-mail-forward

commit 32d813ce44b5531f05f3c5378dd3b519e1cecffb

build: properly use cargo wrapper
to actually get debug symbols with rustc >= 1.77

Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

Makefile
@@ -54,7 +54,7 @@ cargo-build:
 
 install: cargo-build
 	install -dm755 $(DESTDIR)/usr/bin
-	install -m4755 -o root -g root $(COMPILEDIR)/proxmox-mail-forward $(DESTDIR)/usr/bin/proxmox-mail-forward
+	$(CARGO) install
 
 .PHONY: upload
 upload: $(DEBS)

debian/rules
@@ -3,14 +3,18 @@
 #DH_VERBOSE = 1
 
 include /usr/share/dpkg/pkg-info.mk
+include /usr/share/rustc/architecture.mk
 
 export BUILD_MODE=release
 
-CARGO=/usr/share/cargo/bin/cargo
-
 export CFLAGS CXXFLAGS CPPFLAGS LDFLAGS
 export DEB_HOST_RUST_TYPE DEB_HOST_GNU_TYPE
+
+export CARGO=/usr/share/cargo/bin/cargo
 export CARGO_HOME = $(CURDIR)/debian/cargo_home
+
+export DEB_CARGO_CRATE=proxmox-mail-forward_$(DEB_VERSION_UPSTREAM)
+export DEB_CARGO_PACKAGE=proxmox-mail-forward
 
 %:
 	dh $@
@@ -18,6 +22,7 @@ export CARGO_HOME = $(CURDIR)/debian/cargo_home
 override_dh_auto_configure:
 	@perl -ne 'if (/^version\s*=\s*"(\d+(?:\.\d+)+)"/) { my $$v_cargo = $$1; my $$v_deb = "$(DEB_VERSION_UPSTREAM)"; \
 	    die "ERROR: d/changelog <-> Cargo.toml version mismatch: $$v_cargo != $$v_deb\n" if $$v_cargo ne $$v_deb; exit(0); }' Cargo.toml
+	$(CARGO) prepare-debian $(CURDIR)/debian/cargo_registry --link-from-system
 	dh_auto_configure
 
 override_dh_fixperms:

commit 9d10d3d26081660bf690cef8f1bd6dab7b67a35b

buildsys: set setuid bit in installed proxmox-mail-forward binary
A previous commit removed the part which sets the bit.
Without the bit we cannot read the notification config, because
Postfix invokes forwarding binaries as 'nobody'.

Fixes: 32d813ce44b ("build: properly use cargo wrapper")
Signed-off-by: Lukas Wagner <l.wagner@proxmox.com>

Makefile
@@ -55,6 +55,7 @@ cargo-build:
 install: cargo-build
 	install -dm755 $(DESTDIR)/usr/bin
 	$(CARGO) install
+	chmod u+s $(DESTDIR)/usr/bin/$(PACKAGE)
 
 .PHONY: upload
 upload: $(DEBS)